cfbfe9d81f68a536891e0e4a8155601b0530f1d4
Srimanthbabugattamaneni <144981709+Srimanthbabugattamaneni@users.noreply.github.com>
2024-03-20 15:44:26 -0400

    logs update

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -7,8 +7,16 @@ const authenticate = require('../middlewares/basicAuth');
 const healthCheck = async (req, res) => {
     try {
         await sequelize.authenticate();
+        logger.info({
+          severity: 'INFO',
+          message: 'Database connection successful'
+        });
         res.status(200).send();
     } catch (error) {
+      logger.error({
+        severity: 'ERROR',
+        message: 'Database connection failed'
+      });
         res.status(503).send();
     }
 };
@@ -19,6 +27,10 @@ const createUser = async (req, res) => {
   
       const existingUser = await User.findOne({ where: { username } });
       if (existingUser) {
+        logger.debug({
+          severity: 'DEBUG',
+          message: 'Attempt to create a user that already exists'
+        });
         return res.status(400).send('User already exists with this username.');
       }
       const hashedPassword = await bcrypt.hash(password, 10);
@@ -30,6 +42,10 @@ const createUser = async (req, res) => {
         account_created: new Date(),
         account_updated: new Date()
       });
+      logger.info({
+        severity: 'INFO',
+        message: 'user created sucessfully'
+      });
       res.status(201).json({
         id: user.id,
         username: user.username,
@@ -40,6 +56,10 @@ const createUser = async (req, res) => {
       });
     } catch (error) {
        console.error(error);
+       logger.error({
+        severity: 'ERROR',
+        message: 'User creation failed'
+      });
       res.status(400).send('please enter valid details');
     }
 };
@@ -52,6 +72,10 @@ const getUser = async (req, res) => {
           where: { username: basic.name } 
         });
         if (user === null) {
+          logger.error({
+            severity: 'ERROR',
+            message: 'Auth not provided correctly'
+          });
           return res.status(400).json({ error: "User Not Found" });
       } else {
         const auth = await authenticate(req, user);
@@ -61,16 +85,28 @@ const getUser = async (req, res) => {
         if('password' in users){
             delete users.password;
           }
+          logger.info({
+            severity: 'INFO',
+            message: 'Auth successfull'
+          });
 
           
           res.status(200).json(users);
         }
         else{
+          logger.warn({
+            severity: 'WARN',
+            message: 'unauthorised  user'
+          });
           res.status(401).send("Unauthorised User");
         }
       }
       } catch (error) {
         console.error(error);
+        logger.error({
+          severity: 'ERROR',
+          message: 'details not provided correctly'
+        });
         res.status(400).json('incorrect username and password');
       }
 };
@@ -80,12 +116,20 @@ const updateUser = async (req, res) => {
 
  
   if (!first_name && !last_name && !password) {
+    logger.info({
+      severity: 'INFO',
+      message: 'update successfull'
+    });
       return res.status(204).send();
   }
 
   try {
       const user = await User.findOne({ where: { username } });
       if (!user) {
+        logger.warn({
+          severity: 'WARN',
+          message: 'User not found'
+        });
           return res.status(404).send('User not found.');
       } else {
           const auth = await authenticate(req, user);
@@ -112,14 +156,27 @@ const updateUser = async (req, res) => {
                   await user.save();
                   res.send('User updated successfully.');
               } else {
+                logger.info({
+                  severity: 'INFO',
+                  message: 'update successfull'
+                });
+
                   return res.status(204).send();
               }
           } else {
+            logger.warn({
+              severity: 'WARN',
+              message: 'Unauthorized User of update'
+            });
               res.status(401).send("Unauthorized User");
           }
       }
   } catch (error) {
       console.error(error);
+      logger.error({
+        severity: 'ERROR',
+        message: 'error updating the user'
+      });
       res.status(500).send('Error updating user.');
   }
 };
--- model/user.js
+++ model/user.js
@@ -2,6 +2,12 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.UUID,
+    primaryKey: true,
+    defaultValue: DataTypes.UUIDV4,
+  },
+  
   username: {
     type: DataTypes.STRING,
     allowNull: false,
