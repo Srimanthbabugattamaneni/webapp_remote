41bda1d4d349370a0d28f0ed34d27285e10caf4d
Srimanthbabugattamaneni <144981709+Srimanthbabugattamaneni@users.noreply.github.com>
2024-03-20 15:44:26 -0400

    logs update

--- packer/setup.sh
+++ packer/setup.sh
@@ -35,12 +35,58 @@ sudo chmod -R 750 /opt
 
 # Install Node.js dependencies and run tests
 sudo npm install
-sudo npm test
+
+sudo curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+sudo bash add-google-cloud-ops-agent-repo.sh --also-install
+ 
+logger=$(cat << EOF
+logging:
+  receivers:
+    webapp-receiver:
+      type: files
+      include_paths:
+        - /var/log/webapp/webapp.log
+      record_log_file_path: true
+  processors:
+    webapp-processor:
+      type: parse_json
+      time_key: time
+      time_format: "YYYY-MM-DDTHH:mm:ss.SSSZ"
+    move_severity:
+      type: modify_fields
+      fields:
+        severity:
+          move_from: jsonPayload.severity
+  service:
+    pipelines:
+      default_pipeline:
+        receivers: [webapp-receiver]
+        processors: [webapp-processor, move_severity]
+EOF
+)
+ 
+# Backup the old config.yml file
+sudo cp /etc/google-cloud-ops-agent/config.yaml /etc/google-cloud-ops-agent/config.yaml.backup
+ 
+# Create a new config.yml file
+echo "$logger" | sudo tee /etc/google-cloud-ops-agent/config.yaml >/dev/null
+ 
+# Set permissions for the new config.yml file
+sudo chown -R csye6225:csye6225 /etc/google-cloud-ops-agent/config.yaml
+ 
+sudo mkdir -p /var/log/webapp
+ 
+sudo touch /var/log/webapp/webapp.log
+ 
+sudo chown -R csye6225:csye6225 /var/log/webapp
+ 
+sudo systemctl enable google-cloud-ops-agent
+sudo systemctl start google-cloud-ops-agent
+sudo systemctl restart google-cloud-ops-agent
 
 # Reload systemd, enable, and start Apache HTTP Server
 sudo systemctl daemon-reload
-sudo systemctl enable httpd
-sudo systemctl start httpd
+
 
 # Enable and start the custom service
 sudo systemctl enable csye6225
